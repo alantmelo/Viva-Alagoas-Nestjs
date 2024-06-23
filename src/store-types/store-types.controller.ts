700d2fa822e7d5a16d7309ea712e55c6f5755c72
Alan Melo <alantmelo@gmail.com>
2024-06-23 13:03:28 -0300

    [feat]: add store type resource

--- src/store-types/store-types.controller.ts
+++ src/store-types/store-types.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { StoreTypesService } from './store-types.service';
 import { CreateStoreTypeDto } from './dto/create-store-type.dto';
 import { UpdateStoreTypeDto } from './dto/update-store-type.dto';
@@ -23,7 +31,10 @@ export class StoreTypesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStoreTypeDto: UpdateStoreTypeDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateStoreTypeDto: UpdateStoreTypeDto,
+  ) {
     return this.storeTypesService.update(+id, updateStoreTypeDto);
   }
 
